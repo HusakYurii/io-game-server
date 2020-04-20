ded1042f6f7287eb17436a561586a5a08b62223a
Husak Yurii <gusak.1881@gmail.com>
2020-04-20 23:46:22 +0200

    ConnectionManager class was used (Closes #1)

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 const path = require('path');
-
 const Express = require('express');
 const expressApp = Express();
 const httpServer = require('http').createServer(expressApp);
 const socketIo = require('socket.io')(httpServer);
 
+const ConnectionManager = require('./js/ConnectionManager.js');
+
 const PORT = process.env.PORT || 9090;
 
 expressApp.use(Express.static(path.join(__dirname, '../dist/client')));
 
-socketIo.on('connection', (clientSocket) => {
-    console.log(`A new user has been connected`);
-    
-    clientSocket.on('disconnect', () => {
-        console.log(`A user has been disconnected`);
-    });
-});
+const connectionManager = new ConnectionManager(socketIo);
+connectionManager.init();
 
 httpServer.listen(PORT, (err) => {
     if (err) {
