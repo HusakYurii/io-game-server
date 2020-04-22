0adee27293e8146f0642cfac37e03f1325e42cd9
Husak Yurii <gusak.1881@gmail.com>
2020-04-23 00:15:31 +0200

    RoomsManager class API was updated to match requirements (Updates #4)

--- js/GameRoomsManager.js
+++ js/GameRoomsManager.js
@@ -11,15 +11,87 @@ class RoomsManager {
         return this.maxRooms === this.rooms.length;
     }
 
+    /**
+     * To delete a player from the related room. 
+     * Player has not room id only at the Login and Game over states.
+     * If, afterwards, the room becomes empty, delete the room
+     * @param {Player} player 
+     */
+    deletePlayerFromRoom(player) {
+        if (!this.hasRoom(player.roomId)) {
+            return;
+        }
+
+        const room = this.getRoomById(player.roomId);
+        room.deletePlayer(player.id);
+        if (room.isEmpty) {
+            this.deleteRoom(room.id);
+        }
+    }
+
+    /**
+     * To add a user to the room using provided roomId otherwise create new room or take an existing one.
+     * In the case when a user sent room id which does not exist, new room will be created, or an existing one will be taken.
+     * @param {Player} player 
+     * @param {{id: string, roomId: string, name: string}} data 
+     * @returns {string} - room id
+     */
+    addPlayerToRoom(player, data) {
+        let roomId = data.roomId.replace(/\s/g, "");
+        let room = this.getRoomById(roomId);
+
+        if (!room || room.isFull) {
+            room = this.getFreeRoom();
+            if (!room) {
+                room = this.createRoom();
+            }
+        }
+
+        player.roomId = room.id;
+        room.addPlayer(player);
+        return room.id;
+    }
+
+    /**
+     * @returns {GameRoom}
+     */
     createRoom() {
         const room = new GameRoom(Tools.generateId());
         this.rooms.set(room.id, room);
+        return room;
     }
 
+    /**
+     * @param {string} roomId 
+     * @returns {boolean}
+     */
     hasRoom(roomId) {
         return this.rooms.has(roomId);
     }
 
+    /**
+     * @param {string} roomId 
+     * @returns {GameRoom | undefined}
+     */
+    getRoomById(roomId) {
+        return this.rooms.get(roomId);
+    }
+
+    /**
+     * To take any room which has free slots
+     * @returns {GameRoom | undefined}
+     */
+    getFreeRoom() {
+        for(let room of this.rooms.values()) {
+            if(!room.isFull) {
+                return room;
+            }
+        } 
+    }
+
+    /**
+     * @param {string} roomId 
+     */
     deleteRoom(roomId) {
         this.rooms.delete(roomId);
     }
