3dcc61f93df81eb92d1be19670e8f367135bed69
Husak Yurii <gusak.1881@gmail.com>
2020-04-23 00:16:31 +0200

    ConnectionManager class API was updated to match requirements (Updates #4)

--- js/ConnectionManager.js
+++ js/ConnectionManager.js
@@ -1,10 +1,11 @@
-const Tools = require('../../shared/Tools.js');
+const GameRoomsManager = require('./GameRoomsManager.js');
 const Player = require('./Player.js');
 
 class ConnectionManager {
     constructor(connectionSocket) {
-        this.idlePool = new Map();
+        this.connectionsPool = new Map();
         this.connectionSocket = connectionSocket;
+        this.gameRoomsManager = new GameRoomsManager();
     }
 
     init() {
@@ -12,20 +13,37 @@ class ConnectionManager {
     }
 
     onConnection(playerSocket) {
-        const player = new Player(playerSocket, Tools.generateId());
-        this.idlePool.set(player.id, player);
+        const player = new Player(playerSocket, playerSocket.id);
+        this.connectionsPool.set(player.id, player);
 
         player.emit("user-connected", { id: player.id });
         player.on("disconnect", this.onDisconnection.bind(this, player.id));
+        player.on("login-user", this.onPlayerLogin.bind(this));
     }
 
     onDisconnection(playerId) {
-        const player = this.idlePool.get(playerId);
-        this.idlePool.delete(playerId);
+
+        const player = this.connectionsPool.get(playerId);
+        this.connectionsPool.delete(playerId);
         player.removeAllListeners();
+
+        this.gameRoomsManager.deletePlayerFromRoom(player);
+    }
+
+    onPlayerLogin(playload) {
+        const data = JSON.parse(playload);
+        const player = this.connectionsPool.get(data.id);
+
+        const newRoomId = this.gameRoomsManager.addPlayerToRoom(player, data);
+        player.emit("user-loggedin", { roomId: newRoomId });
+    }
+
+    isPlayerConnected(playerId) {
+        return this.connectionsPool.has(playerId);
     }
 
     logMessage(message) {
+        if(!DEBUG_EVENTS) return;
         console.log(message);
     }
 }
