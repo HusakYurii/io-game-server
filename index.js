d8ba952637dc69b7e831775b44eca2e65cc1d94c
Husak Yurii <gusak.1881@gmail.com>
2020-06-10 22:42:06 +0200

    stall changes to make it able to set different url for diff env

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ const socketIo = require('socket.io')(httpServer);
 const ConnectionManager = require('./js/ConnectionManager.js');
 
 const PORT = process.env.PORT || 9090;
- 
-// const url = "192.168.168.101";
-// if ( not prod ) {
-//      expressApp.use(Express.static(path.join(__dirname, '../dist/client')));
-//  }
+const url = process.env.NODE_ENV === undefined ? "192.168.168.101" : undefined;
+
+// To host it if client files are on the same website as server
+// expressApp.use(Express.static(path.join(__dirname, '../dist/client')));
+//  
 
 const connectionManager = new ConnectionManager(socketIo);
 connectionManager.init();
 
-httpServer.listen(PORT, (err) => {
+httpServer.listen(PORT, url, (err) => {
     if (err) {
         throw new Error(err);
     }
